e39e8979043fecdaaade6d12b9a247bb5c898df6
amarealcoder <ugorjimiracle06@gmail.com>
2022-07-18 16:59:43 +0100

    Fix external links

--- client/src/Components/MenuItems.jsx
+++ client/src/Components/MenuItems.jsx
@@ -8,24 +8,24 @@ export const MenuItems = [
 		title: "📰 Blog",
 		path: "https://eke.hashnode.dev/rocketmeme-a-meme-library-and-editor-for-creating-and-sharing-memes",
 		cName: "dropdown-link",
-		target: "blank",
+		target: "_blank",
 	},
 	{
 		title: "📄 Documentation",
 		path: "https://github.com/Evavic44/rocketmeme",
 		cName: "dropdown-link",
-		target: "blank",
+		target: "_blank",
 	},
 	{
 		title: "🔐 License",
 		path: "https://github.com/Evavic44/rocketmeme/blob/main/LICENSE",
 		cName: "dropdown-link",
-		target: "blank",
+		target: "_blank",
 	},
 	{
 		title: "⚡ Rocketmeme API",
 		path: "https://rocketteam.gitbook.io/rocketmemes-api/",
 		cName: "dropdown-link",
-		target: "blank",
+		target: "_blank",
 	},
 ];
